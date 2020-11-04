2fc131f39f66f9df3b1af94154236cf570da35e8
Emanuil Manev <Emanuil.Manev@Riscogroup.com>
2020-11-04 17:37:01 +0200

    Third commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContainersCRM.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext()
+        {
+        }
+
+        public ApplicationDbContext(DbContextOptions dbContextOptions)
+            : base(dbContextOptions)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=.;Database=ContainersCRM;Integrated Security=true;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.Entity<UserCard>().HasKey(x => new { x.UserId, x.CardId });
+            base.OnModelCreating(modelBuilder);
+        }
+
+      
+    }
+}
